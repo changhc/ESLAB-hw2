9283da6ecdf1eebf2b94e87d76288abc08b4a8ed
ken2576 <kaien.lin@gmail.com>
2017-05-04 18:13:47 +0800

    Merged backend.

---
+++ server/server.js
@@ -0,0 +1,117 @@
+'use strict'
+
+const restify = require('restify');
+const request = require('request');
+const path = require('path');
+const server = restify.createServer();
+let ipList = {};
+let database = {};
+server.use(restify.bodyParser());
+server.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+server.listen(3333, () => {
+  console.log('%s listening to %s', server.name, server.url);
+});
+
+server.get('/api/realTime', (req, res) => {
+  let body = {
+    timestamp: Date.now(),
+    data: [],
+  };
+  for (let key in database) {
+    if (database.hasOwnProperty(key)) {
+      let deviceData = database[key][database[key].length - 1];
+      deviceData.deviceId = key;
+      deviceData.coordE = ipList[key].coordE;
+      deviceData.coordN = ipList[key].coordN;
+      body.data.push(deviceData);
+    }
+  }
+  res.send(200, JSON.stringify(body));
+});
+
+server.post('/api/histData', (req, res) => {
+  const id = req.body.deviceId;
+  try {
+    if (!ipList.hasOwnProperty(id)) {
+      throw Error('bad request');
+    }
+  } catch (err) {
+    console.error(err);
+    res.send(400);
+  }
+
+  const body = {
+    deviceId: id,
+    histData: [],
+  };
+  const endIndex = database[id].length;
+  const dataCount = endIndex < 10 ? endIndex : 10;
+  for (let i = dataCount; i > 0; --i) {
+    body.histData.push(database[id][endIndex - i]);
+  }
+  res.send(200, JSON.stringify(body));
+});
+
+server.post('/api/register', (req, res) => {
+  const ip = req.connection.remoteAddress.replace('::ffff:', '');
+  const deviceId = req.body.deviceId;
+  if (!ipList[deviceId] || ipList[deviceId] !== ip) {
+    console.log(`new device: ${deviceId}`);
+    ipList[deviceId] = { ip: ip, coordN: req.body.coordN, coordE: req.body.coordE };
+  }
+  res.send(200);
+});
+
+server.post('/api/device', (req, res) => {
+  const deviceId = req.body.deviceId;
+  if (!ipList.hasOwnProperty(deviceId)) {
+    res.send(403, 'no registration');
+  }
+  try {
+    if (parseInt(deviceId, 10) < 0) {
+      throw Error('bad request');
+    }
+    if (parseFloat(deviceId) !== parseInt(deviceId, 10)) {
+      throw Error('bad request');
+    }
+    if (!database[deviceId]) {
+      database[deviceId] = [];
+    }
+    database[deviceId].push({
+      timestamp: req.body.timestamp,
+      temp: req.body.temp,
+      humidity: req.body.humidity,
+      servoSpeed: req.body.servoSpeed,
+    });
+    res.send(202);
+  } catch (err) {
+    console.log(err);
+    res.send(400);
+  }
+});
+
+server.put('/api/control', (req, res) => {
+  try{
+    const key = req.body.deviceId;
+    if (ipList.hasOwnProperty(key)) {
+      console.log(ipList[key]);
+      request.put({
+        headers: { 'content-type': 'application/json' },
+        url: `http://${ipList[key].ip}:9999/command`,
+        body: JSON.stringify(req.body),
+      }, (err, res2, body) => {
+        if (err) console.log(err);
+      });
+    }
+    res.send(200);
+  } catch (err) { console.log(err); res.send(500, err); }
+});
+
+server.get(/\/*/, restify.serveStatic({
+  directory: path.join(__dirname, '/src'),
+  default: 'index.html',
+}));
---
+++ tessel/index.js
@@ -0,0 +1,132 @@
+// Import the interface to Tessel hardware
+var tessel = require('tessel');
+var request = require('request');
+var restify = require('restify');
+var climateLib = require('climate-si7020');
+var servoLib = require('servo-pca9685');
+
+var climate = climateLib.use(tessel.port['A']);
+var servo = servoLib.use(tessel.port['B']);
+var servo1 = { pin: 1, deviceId: require('os').networkInterfaces().eth0[0].mac, position: 0, speed: 0 };
+var servoToggle = null;
+var sender = null;
+var requester = null;
+
+var remoteDomain = 'http://192.168.0.101:3333';
+climate.on('ready', function () {
+  console.log('Connected to climate module');
+  requester = setInterval(() => sendRequest(), 5000);
+});
+
+climate.on('error', function(err) {
+  console.log('error connecting module', err);
+});
+
+servo.on('ready', function() {
+  console.log('Servo ready');
+});
+
+servo.on('error', function(err) {
+  console.error('Servo error', err);
+});
+
+const sendRequest = () => {
+  request.post({
+    headers: { 'content-type': 'application/json' },
+    url: remoteDomain + '/api/register',
+    body: JSON.stringify({
+      deviceId: servo1.deviceId,
+      coordN: 25.0030115,
+      coordE: 121.4823595,
+    }),
+  }, (err, res, body) => {
+    try {
+      if (res.statusCode > 300) {
+        console.log(res.statusCode, body);
+        return;
+      }
+      clearInterval(requester);
+      sender = setInterval(function () {
+        climate.readTemperature('c', function (err, temp) {
+          climate.readHumidity(function (err, humid) {
+            console.log(`Tessel ID ${servo1.deviceId}\tTemp: ${temp.toFixed(4)}C, Humidity: ${humid.toFixed(4)}%RH`);
+            request.post({
+              headers: { 'content-type': 'application/json' },
+              url: remoteDomain + '/api/device',
+              body: JSON.stringify({
+                timestamp: Date.now(),
+                deviceId: servo1.deviceId,
+                temp: temp,
+                humidity: humid,
+                servoSpeed: servo1.speed,
+              }),
+            }, (err, res, body) => {
+              try {
+                if (res.statusCode > 300) {
+                  console.log(res.statusCode);
+                  clearInterval(sender);
+                  stopServo();
+                  requester = setInterval(() => sendRequest(), 5000);
+                }
+              } catch (error) {
+                console.log(error);
+              }
+            });
+          });
+        });
+      }, 5 * 1000);
+    } catch (error) {
+      console.log(error);
+    }
+  });
+};
+
+const startServo = () => {
+  try {
+    servo.move(servo1.pin, servo1.position);
+    servo1.position += 0.1;
+    if (servo1.position > 1) {
+      servo1.position = 0;
+    }
+
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+const stopServo = () => {
+  servo1.speed = 0;
+  if (servoToggle !== null) {
+    clearInterval(servoToggle);
+  }
+};
+// server part
+var server = restify.createServer();
+server.use(restify.bodyParser());
+server.put('/command', (req, res) => {
+  console.log(req.body);
+  const body = req.body;
+  if (body.type === 'servo' && !servo._connected) {
+    res.send(500, 'Servo not connected');
+  }
+  if (body.deviceId !== servo1.deviceId) {
+    res.send(404, 'DeviceId mismatch');
+  }
+  if (body.type === 'servo' && body.value === 'on') {
+    if (servo1.speed === 0) {
+      servo1.speed = 0.1;
+      servoToggle = setInterval(() => startServo(), 500);
+    }
+  } else if (body.type === 'servo' && body.value === 'off') {
+    stopServo();
+  }
+  res.send(200);
+});
+
+server.get('/test', (req, res) => {
+  console.log(req.body);
+});
+
+server.listen(9999, () => {
+  console.log('Server running at %s', server.url);
+});
